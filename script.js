e8ea93ee07f56eb0d39bac4028bcdf6299809d27
Amanueldaniel94 <amanueldaniel45@gmail.com>
2025-04-02 19:39:50 +0300

    final
    final

--- script.js
+++ script.js
@@ -1,53 +1,6 @@
-// let timer;
-// let isRunning = false;
-// let seconds = 0, minutes = 0, hours = 0;
 
-// function updateDisplay() {
-//     document.querySelector(".stopwatch").textContent = 
-//         (hours < 10 ? "0" : "") + hours + ":" +
-//         (minutes < 10 ? "0" : "") + minutes + ":" +
-//         (seconds < 10 ? "0" : "") + seconds;
-// }
 
-// function start() {
-//     if (!isRunning) {
-//         isRunning = true;
-//         timer = setInterval(() => {
-//             seconds++;
-//             if (seconds == 60) { seconds = 0; minutes++; }
-//             if (minutes == 60) { minutes = 0; hours++; }
-//             updateDisplay();
-//         }, 1000);
-//     }
-// }
-
-// function pause() {
-//     clearInterval(timer);
-//     isRunning = false;
-// }
-
-// function reset() {
-//     clearInterval(timer);
-//     isRunning = false;
-//     seconds = 0;
-//     minutes = 0;
-//     hours = 0;
-//     updateDisplay();
-//     document.getElementById("laps").innerHTML = "";
-// }
-
-// function lap() {
-//     if (isRunning) {
-//         let lapTime = document.createElement("li");
-//         lapTime.textContent = document.querySelector(".stopwatch").textContent;
-//         document.getElementById("laps").appendChild(lapTime);
-//     }
-// }
-
-// updateDisplay();
-
-let timer;
-let isRunning = false;
+let timer;let isRunning = false;
 let microseconds = 0, seconds = 0, minutes = 0, hours = 0;
 
 function updateDisplay() {
